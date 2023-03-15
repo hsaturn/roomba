a3db7e89d75e954b631277d931505eb07e660a06
Francois BIOT <hsaturn@gmail.com>
2023-03-15 19:06:05 +0100

    [nanos] Added every command

--- src/nanos.cpp
+++ src/nanos.cpp
@@ -14,6 +14,7 @@ Nanos::Nanos()
 {
     handlers = {
         { "reboot", { "" ,                  [](Params& p)->bool { ESP.reset(); return true; }}},
+        { "echo",   { "" ,                  [](Params& p)->bool { p.out << p.args.c_str() << endl; p.args.clear(); return true; }}},
         { "ip",     { "" ,                  [](Params& p)->bool { p.out << "ip: " << WiFi.localIP().toString() << endl; return true; }}},
         { "free", { "" ,                    [](Params& p)->bool { p.out << "free heap: " << String(ESP.getFreeHeap()) << endl; return true; }}},
         { "error", { "n : set flash mode" , [](Params& p)->bool { flash.error(getInt(p.args)); return true; }}},
@@ -36,20 +37,88 @@ Nanos::Nanos()
             max_loop_ = 0;
             return true;
         }}},
+        { "every", { "ms command | on | off | list | remove #", [this](Params& p)->bool { this->every(p); return true; }}},
+        { "pin", { "n , read | in | ind | fun | out" , [](Params& p)->bool
+            {
+                uint8_t pin = getInt(p.args);
+                std::string mode = getWord(p.args);
+                p.out << "pin " << pin << ' ';
+                if (mode == "read") { p.out << (digitalRead(pin)==LOW ? "low" : "high"); }
+                else if (mode == "fun") { pinMode(pin, FUNCTION_3); p.out  << ": function_3"; }
+                else if (mode == "in") { pinMode(pin, INPUT); p.out << ": input"; }
+                else if (mode =="ind") { pinMode(pin, INPUT_PULLDOWN_16); p.out << ": input pulldown"; }
+                else if (mode == "out") { pinMode(pin, OUTPUT); p.out << ": output"; }
+                else { p.out << '?'; }
+                p.out << endl;
+                return true;
+            }}},
+        { "toggle", { "" , [](Params& p)->bool
+            {
+                uint8_t pin = getInt(p.args);
+                digitalWrite(pin, digitalRead(pin) == LOW ? HIGH : LOW);
+                return true;
+            }}},
     };
+}
+
+void Nanos::every(Params& p)
+{
+    p.out << "every: " << p.args.c_str() << endl;
+    std::string cmd = getWord(p.args);
+    if (cmd == "on")
+    {
+        every_on_ = true;
+    }
+    else if (cmd == "off")
+    {
+        every_on_ = false;
+    }
+    else if (cmd == "remove")
+    {
+        uint32_t index = getInt(p.args);
+        if (index < every_.size())
+            every_.erase(every_.begin()+index);
+    }
+    else if (cmd == "list")
+    {
+        int index=0;
+        p.out << "Every list, size=" << every_.size() << endl;
+        p.out << "------------" << endl;
+        for(const auto& e: every_)
+        {
+            p.out << '#' << index++ << ' ';
+            e.dump(p.out);
+            p.out << endl;
+        }
+        p.out << "------------" << endl;
+    }
+    else
+    {
+        int delay = getInt(cmd);
+        if (delay == 0)
+        {
+            p.out << "ERROR : expecting number" << endl;
+            return;
+        }
+        Every e;
+        e.active = true;
+        e.cmd = p.args;
+        e.ms = delay;
+        e.next = millis() + delay;
+        every_.push_back(e);
+    }
 }
 
 void Nanos::def(Params& p)
 {
     std::string name = getWord(p.args);
     std::string cmd = p.args;
+    p.out << "cmd=(" << cmd.c_str() << ")" << endl;
     handlers[name] = { "", [this, cmd](Params& q) -> bool
         {
             if (++recurse < 20)
             {
                 q.args = cmd;
-                std::string tmp(q.args);
-                q.out << "exec " << tmp.c_str() << endl;
                 Command::handle(q);
             }
             else
@@ -71,13 +140,27 @@ void Nanos::undef(Params& p)
 
 void Nanos::loop()
 {
-    auto us = micros();
+    auto ms = millis();
     if (last_loop_)
     {
-        auto delta_us = us-last_loop_;
-        loops_avg_ = 0.99*loops_avg_ + 0.01*delta_us;
-        max_loop_ = std::max(delta_us, max_loop_);
-        min_loop_ = std::min(delta_us, min_loop_);
+        auto delta_ms = ms-last_loop_;
+        loops_avg_ = 0.99*loops_avg_ + 0.01*delta_ms;
+        max_loop_ = std::max(delta_ms, max_loop_);
+        min_loop_ = std::min(delta_ms, min_loop_);
     }
-    last_loop_ = us;
+    last_loop_ = ms;
+}
+
+std::string Nanos::getEvery()
+{
+    auto ms = millis();
+    for(auto& e: every_)
+    {
+        if (ms >= e.next)
+        {
+            e.next += e.ms;
+            return e.cmd;
+        }
+    }
+    return "";
 }
--- src/nanos.h
+++ src/nanos.h
@@ -2,6 +2,26 @@
 
 #include "command.h"
 
+struct Every
+{
+  std::string cmd;
+  uint32_t ms;
+  uint32_t next;
+  bool active=true;
+
+  void dump(OutputStream& out) const
+  {
+		out << (active ? "enabled " : "disabled ");
+
+    auto mill=millis();
+    out << ms << "ms [" << cmd.c_str() << "] next in ";
+    if (mill > next)
+      out << "now";
+    else
+      out << next-mill << "ms";
+  }
+};
+
 class Nanos : public Command
 {
 	public:
@@ -9,13 +29,19 @@ class Nanos : public Command
 		const char* name() const override { return "nanos"; }
 
 		void loop() override;
+		std::string getEvery();
 
 	private:
 		void def(Params&);
 		void undef(Params&);
 		char recurse = 0;
-		float loops_avg_ = 0;	// avg delay between loops in us
+		float loops_avg_ = 0;	// avg delay between loops in ms
 		unsigned long last_loop_ = 0;
 		unsigned long min_loop_ = 999999;
 		unsigned long max_loop_ = 0;
+		bool every_on_ = true;
+
+		void every(Params&);
+
+		std::vector<Every> every_;
 };
